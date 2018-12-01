20a9b1f40f46dc5c675c4f8570fd669954a23430
Zygios <zygissav@gmail.com>
2018-12-01 07:38:37 +0200

    small fix :)

--- ESP32_Paradox_controler_mqtt.ino
+++ ESP32_Paradox_controler_mqtt.ino
@@ -17,8 +17,8 @@
 
 HardwareSerial paradoxSerial(2);
 
-const char* ssid = "Namai40";
-const char* password = "Zygios5505";
+const char* ssid = "xxxx";
+const char* password = "xxxx";
 #define mqtt_server       "192.168.1.40"
 #define mqtt_port         1883
 const String ParadoxLoginPassword = "1234";
