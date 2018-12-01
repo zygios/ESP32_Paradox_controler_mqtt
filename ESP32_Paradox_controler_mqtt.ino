9e340f34b3a6aecfee3f0cf91c0b6ff41a38ba9d
Zygios <zygissav@gmail.com>
2018-12-01 07:33:36 +0200

    now working arming, disarm, put in sleep/stay mode Paradox from Domoticz server.

--- ESP32_Paradox_controler_mqtt.ino
+++ ESP32_Paradox_controler_mqtt.ino
@@ -17,15 +17,35 @@
 
 HardwareSerial paradoxSerial(2);
 
-const char* ssid = "xxxx";
-const char* password = "xxxx";
+const char* ssid = "Namai40";
+const char* password = "Zygios5505";
 #define mqtt_server       "192.168.1.40"
 #define mqtt_port         1883
+const String ParadoxLoginPassword = "1234";
 
 const char *paradox_topicOut = "domoticz/in";
 const char *paradox_topicStatus = "paradox/status";
 const char *paradox_topicIn = "paradox/in";
 const char *paradoxCmdMsg = "DomParadox_Status";
+
+bool PannelConnected =false;
+bool PanelError = false;
+bool ParadoxRunning = false;
+byte ParadoxConfirStatus;
+#define MessageLength 37
+const byte ParadoxDisarm = 11;
+const byte ParadoxArm = 12;
+const byte ParadoxStay = 3;
+const byte ParadoxSleep = 4;
+const byte ParadoxExit = 14;
+const byte ParadoxEntry = 13;
+//-----------------------------;
+const byte DomoticzDisarm = 0;
+const byte DomoticzArm = 10;
+const byte DomoticzStay = 50;
+const byte DomoticzSleep = 40;
+const byte DomoticzExit = 20;
+const byte DomoticzEntry = 30;
 
 WiFiClient espClient;
 PubSubClient mqtt(espClient);
@@ -48,12 +68,12 @@ IdxMap[] = {
 
 static const struct{const int paradox_status, domoticz_status;} 
 StatusMap[] = {
-  { 11, 0 }, //paradox disarmed
-  { 12, 10 }, //paradox armed
-  { 13, 30}, //paradox in entry delay mode
-  { 14, 20 }, //paradox in exit delay mode
-  { 3, 50 }, //paradox in armed stay mode
-  { 4, 40 }  //paradox in armed sleep mode
+  { ParadoxDisarm, DomoticzDisarm }, //paradox disarmed
+  { ParadoxArm, DomoticzArm }, //paradox armed
+  { ParadoxEntry, DomoticzEntry}, //paradox in entry delay mode
+  { ParadoxExit, DomoticzExit }, //paradox in exit delay mode
+  { ParadoxStay, DomoticzStay }, //paradox in armed stay mode
+  { ParadoxSleep, DomoticzSleep }  //paradox in armed sleep mode
 };
 
 struct inPayload
@@ -92,7 +112,6 @@ void setup() {
  
 void loop()
 {
-  //mqtt.loop();
   readSerial();
   if ( (inData[0] & 0xF0)!=0xE0){ // re-align serial buffer
     paradoxSerial_flush_buffer();
@@ -122,7 +141,6 @@ void paradoxSerial_flush_buffer(){
 
 
 void callback(char* topic, byte* payloadmsg, unsigned int length) {
- 
   debug("Message arrived in topic: ");
   debugln(topic);
  
@@ -145,10 +163,63 @@ void callback(char* topic, byte* payloadmsg, unsigned int length) {
     debugln(F("parseObject() failed"));
     sendMQTT(paradox_topicStatus,"Domoticz JSON parseObject() failed.");
   }else{
-    String Domoticz_status = data["DomParadox_Status"];
+    byte Domoticz_status = data["DomParadox_Status"];
     debugln(Domoticz_status);
-  }
+
+    byte ExecCommand;
+    
+    switch (Domoticz_status){    
+      case DomoticzDisarm:
+        debugln(F("Received command to disarm Paradox."));
+        ExecCommand = 0x05;
+        ParadoxConfirStatus = ParadoxDisarm;
+        break;
+      case DomoticzArm:
+        debugln(F("Received command to arm Paradox."));
+        ExecCommand = 0x04;
+        ParadoxConfirStatus = ParadoxArm;
+        break;
+      case DomoticzSleep:
+        debugln(F("Received command to arm in sleep mode Paradox."));
+        ExecCommand = 0x03;
+        ParadoxConfirStatus = ParadoxSleep;
+        break;
+      case DomoticzStay:
+        debugln(F("Received command to arm in stay mode Paradox."));
+        ExecCommand = 0x01;
+        ParadoxConfirStatus = ParadoxStay;
+        break;
+      default:
+        debugln(F("Received incorrect command!!!"));
+        ExecCommand = 0xFF;
+        sendMQTT(paradox_topicStatus,"Received incorrect command from Domoticz!!!");
+        break;
+    }
+
+    if(PannelConnected == false){
+      doLogin();
+    }
   
+    int cnt = 0;
+    while (!PannelConnected && cnt < 10)
+    {
+      readSerial();
+      cnt++;    
+    }
+    
+    if (!PannelConnected)
+    {
+      debugln("Problem connecting to panel");
+      sendMQTT(paradox_topicStatus, "Problem connecting to panel");
+    } else if (PannelConnected && ExecCommand != 0xFF)  {
+        //Executing command on Paradox
+        ExecParadoxCommand(ExecCommand, 0);
+    }else  {
+      debugln("Bad Command ");
+      sendMQTT(paradox_topicStatus, "Bad Command ");
+    }
+    
+  }
 }
 
 boolean reconnect() {
@@ -195,8 +266,34 @@ void readSerial() {
       String retval = "{ \"armstatus\":" + String(paradox.armstatus) + ", \"event\":" + String(paradox.event) + ", \"sub_event\":" + String(paradox.sub_event) + ", \"dummy\":\"" + String(paradox.dummy) + "\"}";
       debugln(retval);
       //sending parsed data to mqtt server
-      SendJsonString(paradox.armstatus, paradox.event, paradox.sub_event, paradox.dummy);
+      debugln("Running:"+String(ParadoxRunning));
+      debugln("ParadoxConfirStatus:"+String(ParadoxConfirStatus));
+      if(ParadoxRunning == true){
+        if( paradox.event == 2 && paradox.sub_event == ParadoxConfirStatus){
+          ParadoxRunning = false;
+          debugln("Gavome statusa koki siunteme!!!");
+        }else if ( paradox.event == 6 && paradox.sub_event == ParadoxConfirStatus){
+          ParadoxRunning = false;
+          debugln("Paradox is in SLEEP/STAY!!!");
+        }else{
+          debugln("Not sending status to Domoticz");
+        }
+      }else{
+        SendJsonString(paradox.armstatus, paradox.event, paradox.sub_event, paradox.dummy);
+      }
     } 
+    if (inData[7] == 48 && inData[8] == 3)
+    {
+      PannelConnected = false;
+      debugln("panel logout");
+      sendMQTT(paradox_topicStatus, "panel logout");
+    }
+    if (inData[7] == 48 && inData[8] == 2 && !PannelConnected)
+    {
+      PannelConnected = true;
+      debugln("panel Login");
+      sendMQTT(paradox_topicStatus, "panel Login");
+    }
   }
  
 }
@@ -245,21 +342,21 @@ void SendJsonString(byte armstatus, byte event,byte sub_event  ,String dummy)
       mqtt_msg = "{ \"idx\" : "+String(get_Domoticz_Idx(sub_event))+", \"nvalue\" : "+String(event)+"}";
       break;
     case 2:
-      if(sub_event == 11 || sub_event == 12 || sub_event == 13 || sub_event == 14){
+      if(sub_event == ParadoxDisarm || sub_event == ParadoxArm) {
         mqtt_msg = "{ \"command\": \"switchlight\", \"idx\":"+String(get_Domoticz_Idx(11))+",\"nvalue\" : 1, \"switchcmd\": \"Set Level\", \"level\":\""+String(get_Domoticz_Status(sub_event))+"\"}";
       }
       break;
     case 3:
       if(sub_event == 0){
+        //Paradox siren off
         mqtt_msg = "{ \"idx\" : "+String(get_Domoticz_Idx(10))+", \"nvalue\" : 0}";
       }else if(sub_event == 1){
+        //Paradox siren on
         mqtt_msg = "{ \"idx\" : "+String(get_Domoticz_Idx(10))+", \"nvalue\" : 1}";
       }
       break;
     case 6:
-      if(sub_event == 3){
-        mqtt_msg = "{ \"command\": \"switchlight\", \"idx\":"+String(get_Domoticz_Idx(11))+",\"nvalue\" : 1, \"switchcmd\": \"Set Level\", \"level\":\""+String(get_Domoticz_Status(sub_event))+"\"}";
-      }else if(sub_event == 4){
+      if(sub_event == ParadoxStay || sub_event == ParadoxSleep){
         mqtt_msg = "{ \"command\": \"switchlight\", \"idx\":"+String(get_Domoticz_Idx(11))+",\"nvalue\" : 1, \"switchcmd\": \"Set Level\", \"level\":\""+String(get_Domoticz_Status(sub_event))+"\"}";
       }
       break;
@@ -280,10 +377,9 @@ void SendJsonString(byte armstatus, byte event,byte sub_event  ,String dummy)
       break;
   }
   //passing formated Json string to mqtt function
-  if(mqtt_msg.length() > 0){
+  if(mqtt_msg.length() > 0 ){
     sendMQTT(paradox_topicOut, mqtt_msg); 
   }
-
 }
 
 void sendMQTT(String topicNameSend, String dataStr){
@@ -304,3 +400,194 @@ void sendMQTT(String topicNameSend, String dataStr){
   debugln(topicNameSend);
 }
 
+void doLogin(){
+  debugln("trying to connect to paradox");
+  byte data[MessageLength] = {};
+  byte data1[MessageLength] = {};
+  byte checksum;
+
+  char charpass1[4];
+  char charpass2[4];
+  char charsubcommand[4];
+    
+  String pass1 = ParadoxLoginPassword.substring(0, 2);
+  String pass2 = ParadoxLoginPassword.substring(2, 4);
+  pass1.toCharArray(charpass1, 4);
+  pass2.toCharArray(charpass2, 4);
+
+  unsigned long number1 = strtoul(charpass1, nullptr, 16);
+  unsigned long number2 = strtoul(charpass2, nullptr, 16);
+  
+  byte PanelPassword1 = number1 & 0xFF; 
+  byte PanelPassword2 = number2 & 0xFF; 
+  
+  for (int x = 0; x < MessageLength; x++)
+  {
+      data[x]=0x00;
+      data1[x]=0x00;
+  }
+
+  debugln("Doing login to paradox, flush buffer");
+  paradoxSerial_flush_buffer();
+  data[0] = 0x5f;
+  data[1] = 0x20;
+  data[33] = 0x05;
+  data[34] = 0x00;
+  data[35] = 0x00;
+  data[33] = 0x01;
+
+  checksum = 0;
+  for (int x = 0; x < MessageLength - 1; x++)
+  {
+    checksum += data[x];
+  }
+
+  while (checksum > 255)
+  {
+    checksum = checksum - (checksum / 256) * 256;
+  }
+
+  data[36] = checksum & 0xFF;
+
+//  debug("Data:");
+//  for (int x = 0; x < MessageLength; x++)
+//  {
+//    debug("Address-");
+//    debug(x);
+//    debug("=");
+//    Serial.println(data[x], HEX);
+//  }
+ 
+  debugln("Paradox writing data");
+  paradoxSerial.write(data, MessageLength);
+
+  debugln("Paradox making readSerial command");
+  readSerial();
+  data1[0] = 0x00;
+  data1[4] = inData[4];
+  data1[5] = inData[5];
+  data1[6] = inData[6];
+  data1[7] = inData[7];
+  data1[7] = inData[8];
+  data1[9] = inData[9];
+  data1[10] = 0x00;
+  data1[11] = 0x00;
+  data1[13] = 0x55;
+  data1[14] = PanelPassword1; //panel pc password digit 1 & 2
+  data1[15] = PanelPassword2; //panel pc password digit 3 & 4
+  data1[33] = 0x05;
+
+  checksum = 0;
+  for (int x = 0; x < MessageLength - 1; x++)
+  {
+    checksum += data1[x];
+  }
+  while (checksum > 255)
+  {
+    checksum = checksum - (checksum / 256) * 256;
+  }
+
+  data1[36] = checksum & 0xFF;
+//
+//  for (int x = 0; x < MessageLength; x++)
+//  {
+//    Serial.print("SendinGINITAddress-");
+//    Serial.print(x);
+//    Serial.print("=");
+//    Serial.println(data1[x], HEX);
+//  }
+
+  paradoxSerial.write(data1, MessageLength);
+
+  readSerial();
+//  for (int x = 0; x < MessageLength; x++)
+//  {
+//    Serial.print("lastAddress-");
+//    Serial.print(x);
+//    Serial.print("=");
+//    Serial.println(inData[x], HEX);
+//  }   
+
+  debugln("Finished do login");
+}
+
+void ExecParadoxCommand(byte Command, byte Subcommand){
+  byte checksum;
+  byte armdata[MessageLength] = {};
+  byte exeSubCommand = Subcommand & 0xFF;
+
+  ParadoxRunning = true;
+
+  debugln("Starting ExecParadoxCommand");
+  
+  for (int x = 0; x < MessageLength; x++)
+  {
+    armdata[x] = 0x00;
+  }
+
+  armdata[0] = 0x40;
+  armdata[2] = Command;
+  armdata[3] = exeSubCommand;
+  armdata[33] = 0x01;
+  armdata[34] = 0x00;
+  armdata[35] = 0x00;
+  checksum = 0;
+  
+  for (int x = 0; x < MessageLength - 1; x++)
+  {
+    checksum += armdata[x];
+  }
+    
+  while (checksum > 255)
+  {
+    checksum = checksum - (checksum / 256) * 256;
+  }
+    
+  armdata[36] = checksum & 0xFF;
+
+  while (paradoxSerial.available()>37)
+  {
+    debug(F("serial cleanup"));
+    readSerial();
+  }
+    
+  debugln(F("sending Data to Paradox"));
+  paradoxSerial.write(armdata, MessageLength);
+  //readSerial();
+    
+  if ( inData[0]  >= 40 && inData[0] <= 45)
+  {
+    sendMQTT(paradox_topicStatus, "Command success ");
+    debug(F("Paradox Command executed success "));
+  }
+  debugln("Ending ExecParadoxCommand");      
+}
+
+void PanelDisconnect(){
+  byte data[MessageLength] = {};
+  byte checksum;
+  for (int x = 0; x < MessageLength; x++)
+  {
+    data[x] = 0x00;
+  }
+
+  data[0] = 0x70;
+  data[2] = 0x05;
+  data[33] = 0x01;
+
+  checksum = 0;
+  for (int x = 0; x < MessageLength - 1; x++)
+  {
+    checksum += data[x];
+  }
+
+  while (checksum > 255)
+  {
+    checksum = checksum - (checksum / 256) * 256;
+  }
+
+  data[36] = checksum & 0xFF;
+
+  paradoxSerial.write(data, MessageLength);
+  readSerial();  
+}
